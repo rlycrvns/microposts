7dc5b41a13b4c128c8c3ab888ac6d3c1b90a5b07
rlycrvns <rileycravens@Rileys-MacBook-Pro.local>
2020-09-18 12:35:33 -0700

    add: http module

---
+++ src/http.js
@@ -0,0 +1,50 @@
+class EasyHTTP {
+  // GET Request
+  async get(url) {
+    const response = await fetch(url);
+    const resData = await response.json();
+    return resData;
+  }
+  // POST Request
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    const resData = await response.json();
+    return resData;
+  }
+
+  // PUT Request
+  async put(url, data) {
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    const resData = await response.json();
+    return resData;
+  }
+
+  // DELETE Request
+  async delete(url) {
+    fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+
+    const resData = await 'Resource Deleted';
+    return resData;
+  }
+}
+
+export const http = new EasyHTTP();
