ed17b95ccc1d334600000ee952c1f86590fbf2e9
rlycrvns <rileycravens@Rileys-MacBook-Pro.local>
2020-09-19 16:43:00 -0700

    add: delete post functionality

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ document.addEventListener('DOMContentLoaded', getPosts);
 // listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
 
+// listen for delete
+document.querySelector('#posts').addEventListener('click', deletePost);
+
+//get posts
 function getPosts() {
   http
     .get('http://localhost:3000/posts')
@@ -33,4 +37,21 @@ function submitPost() {
       getPosts();
     })
     .catch((err) => console.log(err));
+}
+
+// delete post
+function deletePost(e) {
+  e.preventDefault();
+  if (e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+    if (confirm('Are you sure?')) {
+      http
+        .delete(`http://localhost:3000/posts/${id}`)
+        .then((data) => {
+          ui.showAlert('Post Removed', 'alert alert-success');
+          getPosts();
+        })
+        .catch((err) => console.log(err));
+    }
+  }
 }
