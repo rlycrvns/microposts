74a5f611e6a3d087dc9a1017a63aa0d67983ce8b
rlycrvns <rileycravens@Rileys-MacBook-Pro.local>
2020-09-19 16:17:14 -0700

    add: submit post functionality

--- src/app.js
+++ src/app.js
@@ -4,9 +4,33 @@ import { ui } from './ui';
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
 function getPosts() {
   http
     .get('http://localhost:3000/posts')
     .then((data) => ui.showPosts(data))
+    .catch((err) => console.log(err));
+}
+
+// Submit post
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body,
+  };
+
+  // create Post
+  http
+    .post('http://localhost:3000/posts', data)
+    .then((data) => {
+      ui.showAlert('Post added', 'alert alert-success');
+      ui.clearFields();
+      getPosts();
+    })
     .catch((err) => console.log(err));
 }
